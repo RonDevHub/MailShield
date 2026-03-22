401b5a0730aa6a4eddd07ff3b27649a40d2f19f5
RonDevHub <no_reply@rondev.de>
2026-03-22 09:45:49 +0000

    src/lang/en.php hinzugefügt

---
+++ src/lang/en.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'title' => 'MailShield 🛡️',
+    'subtitle' => 'Obfuscate your email address to stop bot scrapers.',
+    'input_placeholder' => 'Enter your email...',
+    'btn_generate' => 'Generate Link',
+    'stats_protected' => 'Emails shielded',
+    'stats_since' => 'Shielding since',
+    'copy_success' => 'Copied!',
+    'view_title' => 'Decrypt Email',
+    'view_desc' => 'Solve the captcha to reveal the address.',
+    'invalid_email' => 'Oops, that\'s not a valid email!',
+    'rate_limit' => 'Slow down! Too many requests.',
+    'captcha_fail' => 'Are you a robot? Captcha says yes.',
+    'admin_title' => 'Admin Area',
+    'admin_empty_db' => 'Clear Database'
+];
