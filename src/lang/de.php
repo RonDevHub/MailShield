9ea2df6b862054fcff648c298b2afb60c27be873
RonDevHub <no_reply@rondev.de>
2026-03-22 09:45:11 +0000

    src/lang/de.php hinzugefügt

---
+++ src/lang/de.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'title' => 'MailShield 🛡️',
+    'subtitle' => 'Verschleiere deine E-Mail-Adresse vor Bot-Scrapern.',
+    'input_placeholder' => 'Deine E-Mail eingeben...',
+    'btn_generate' => 'Link erstellen',
+    'stats_protected' => 'Mails geschützt',
+    'stats_since' => 'Beschützt seit',
+    'copy_success' => 'Kopiert!',
+    'view_title' => 'E-Mail entschlüsseln',
+    'view_desc' => 'Löse das Captcha, um die Adresse zu sehen.',
+    'invalid_email' => 'Hoppla, das ist keine gültige E-Mail!',
+    'rate_limit' => 'Sachte! Du hast zu viele Anfragen gestellt.',
+    'captcha_fail' => 'Bist du ein Roboter? Das Captcha sagt ja.',
+    'admin_title' => 'Admin Bereich',
+    'admin_empty_db' => 'Datenbank leeren'
+];
