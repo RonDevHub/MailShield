91a8847a0aff6d1466eb2b8fce776111d1030f53
RonDevHub <no_reply@rondev.de>
2026-03-23 15:44:22 +0000

    src/public/admin.php aktualisiert

--- src/public/admin.php
+++ src/public/admin.php
@@ -130,21 +130,21 @@ if (isset($_SESSION['admin_logged_in'])) {
                         <form method="POST">
                             <button name="export_json" class="w-full bg-blue-600/10 text-blue-500 py-3 rounded-xl font-bold hover:bg-blue-600 hover:text-white transition-all text-sm flex items-center justify-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 512 512"><path fill="currentColor" d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
-                                Exportieren
+                                <?= $lang['export'] ?>
                             </button>
                         </form>
                         <form method="POST" enctype="multipart/form-data" x-ref="jsonForm">
                             <input type="file" name="import_json" class="hidden" x-ref="jsonInput" @change="$refs.jsonForm.submit()">
                             <button type="button" @click="$refs.jsonInput.click()" class="w-full bg-teal-600/10 text-teal-500 py-3 rounded-xl font-bold hover:bg-teal-600 hover:text-white transition-all text-sm flex items-center justify-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 512 512"><path fill="currentColor" d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192l27.5 27.5c25 25 65.5 25 90.5 0L337.5 352H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>
-                                Importieren
+                                <?= $lang['import'] ?>
                             </button>
                         </form>
                     </div>
 
                     <?php if ($driver === 'sqlite'): ?>
                     <form method="POST">
-                        <button name="export_db" class="w-full border border-gray-200 dark:border-gray-700 py-3 rounded-xl text-xs opacity-50 hover:opacity-100 transition-all">SQLite-Datei Backup</button>
+                        <button name="export_db" class="w-full border border-gray-200 dark:border-gray-700 py-3 rounded-xl text-xs opacity-50 hover:opacity-100 transition-all"><?= $lang['sqlite_backup'] ?></button>
                     </form>
                     <?php endif; ?>
 
